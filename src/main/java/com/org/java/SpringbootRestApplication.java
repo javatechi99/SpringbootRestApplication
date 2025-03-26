631f0d6a43d6c2528261423dc6044b878b33e95d
javatechi99 <sreenivasarao.techi@gmail.com>
2025-03-26 16:32:17 +0530

    servlet intilizer added

--- src/main/java/com/org/java/SpringbootRestApplication.java
+++ src/main/java/com/org/java/SpringbootRestApplication.java
@@ -2,14 +2,19 @@ package com.org.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootRestApplication {
+public class SpringbootRestApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		SpringApplication.run(SpringbootRestApplication.class, args);
-		System.out.println("reyf7werwe7");
-		
+			
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootRestApplication.class);
 	}
 
 }
