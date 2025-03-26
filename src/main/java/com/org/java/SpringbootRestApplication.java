9086e943f5fe92e3abe06623026089366259823c
javatechi99 <sreenivasarao.techi@gmail.com>
2025-03-26 16:19:00 +0530

    fdttyfsdf

--- src/main/java/com/org/java/SpringbootRestApplication.java
+++ src/main/java/com/org/java/SpringbootRestApplication.java
@@ -8,6 +8,7 @@ public class SpringbootRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootRestApplication.class, args);
+		System.out.println("reyf7werwe7");
 		
 	}
 
